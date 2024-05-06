ae96fdd246c1dae3561b931b52d04a742d801bd7
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-05-06 12:59:42 +0900

    add: shadcn

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,3 +1,5 @@
+import { Button } from '~/components/ui/button'
+
 export default function Index() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <Button>Hello</Button>
 }
