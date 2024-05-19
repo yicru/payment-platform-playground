b4140cf7c6cb391ead667b246be4138dcaa0d512
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-05-19 14:33:51 +0900

    add: yoga

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello via Bun!')
+import { createSchema, createYoga } from 'graphql-yoga'
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () => 'Hello from Yoga in a Bun app!',
+      },
+    },
+  }),
+})
+
+const server = Bun.serve({
+  fetch: yoga,
+  port: 4000,
+})
+
+console.info(
+  `Server is running on ${new URL(
+    yoga.graphqlEndpoint,
+    `http://${server.hostname}:${server.port}`,
+  )}`,
+)
