1aeba9f379eb7d32064583ecc5beb4aae274d904
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-05-27 12:32:17 +0900

    update: navigation

--- client/app/components/navigation.tsx
+++ client/app/components/navigation.tsx
@@ -1,14 +1,7 @@
 import { Link, useLocation } from '@remix-run/react'
-import { Check, ChevronsUpDownIcon, Slash } from 'lucide-react'
+import { Slash } from 'lucide-react'
 import { Logo } from '~/components/logo'
 import { Button } from '~/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '~/components/ui/dropdown-menu'
 import { cn } from '~/lib/utils'
 
 export function Navigation() {
@@ -33,35 +26,7 @@ export function Navigation() {
         <div className="flex h-10 items-center gap-2">
           <Logo />
           <Slash className="h-6 w-6 -rotate-12 stroke-[1.5px] text-primary/10" />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="ghost"
-                className="gap-2 px-2 data-[state=open]:bg-primary/5"
-              >
-                <span className="h-6 w-6 rounded-full bg-gradient-to-br from-lime-400 from-10% via-cyan-300 to-blue-500" />
-                <p className="text-sm font-medium text-primary/80">USERNAME</p>
-                <ChevronsUpDownIcon className="h-[14px] w-[14px] stroke-[1.5px] text-primary/60" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              sideOffset={8}
-              className="min-w-56 bg-card p-2"
-            >
-              <DropdownMenuLabel className="flex items-center text-xs font-normal text-primary/60">
-                Personal Account
-              </DropdownMenuLabel>
-              <DropdownMenuItem className="h-10 w-full cursor-pointer justify-between rounded-md bg-secondary px-2">
-                <div className="flex items-center gap-2">
-                  <span className="h-6 w-6 rounded-full bg-gradient-to-br from-lime-400 from-10% via-cyan-300 to-blue-500" />
-                  <p className="text-sm font-medium text-primary/80">
-                    USERNAME
-                  </p>
-                </div>
-                <Check className="h-[18px] w-[18px] stroke-[1.5px] text-primary/60" />
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <h1 className={'text-xs font-medium'}>Payment Platform Playground</h1>
         </div>
       </div>
 
