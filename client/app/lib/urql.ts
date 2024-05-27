708f07507be45393c6f5e3b63ec08e1ef5aa3e82
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-05-27 11:56:05 +0900

    add: GraphQL URLを環境変数で指定

--- client/app/lib/urql.ts
+++ client/app/lib/urql.ts
@@ -1,10 +1,10 @@
 import { Client, cacheExchange, fetchExchange } from 'urql'
 
-if (!process.env.VITE_GRAPHQL_URL) {
+if (!import.meta.env.VITE_GRAPHQL_URL) {
   throw new Error('VITE_GRAPHQL_URL is required')
 }
 
 export const client = new Client({
-  url: process.env.VITE_GRAPHQL_URL,
+  url: import.meta.env.VITE_GRAPHQL_URL,
   exchanges: [cacheExchange, fetchExchange],
 })
